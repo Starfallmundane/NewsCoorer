edea2df764c38d7637fedc9794763b01f7b790fc
你个逗比 <1357472058@qq.com>
2020-08-20 12:15:06 +0800

    NewsFragment   newscourr

--- app/src/main/java/com/smm/newscoorer/fragment/NewsFragment.java
+++ app/src/main/java/com/smm/newscoorer/fragment/NewsFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.smm.newscoorer.adapter.HomeAdapter;
+import com.smm.newscoorer.adapter.NewsAdapter;
 import com.smm.newscoorer.bean.BaseBean;
 import com.smm.newscoorer.bean.NewsBean;
 import com.smm.newscoorer.R;
@@ -25,6 +26,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import okhttp3.Call;
 
 public class NewsFragment extends Fragment{
@@ -32,6 +35,8 @@ public class NewsFragment extends Fragment{
     String url = "http://192.168.43.28:8080/zhbj/categories.json";
     ArrayList<String> bannerList ;
     List<NewsBean> list;
+    NewsAdapter newsAdapter;
+    RecyclerView mRecyclerView;
 //    @Override
 //    public void onCreate(@Nullable Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
@@ -43,10 +48,13 @@ public class NewsFragment extends Fragment{
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.newscourr, container, false);
         Log.e("liuxing", "创建--NewsFragment");
+//        数据
         getList();
+//        轮播图
         initNews();
         return view;
     }
+//    轮播图
     public void initNews(){
         initBannerData();//获取数据
 //        轮播图控件设置
@@ -55,6 +63,15 @@ public class NewsFragment extends Fragment{
         banner_home.setAdapter(BannerAdapter);
         //设置轮播图底部小圆点
         banner_home.setIndicator(new CircleIndicator(getActivity()));
+
+//        设置下面新闻列表
+//列表控件
+        newsAdapter = new NewsAdapter(null);
+        mRecyclerView = view.findViewById(R.id.rv_home);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(newsAdapter);
+        newsAdapter.setAnimationEnable(true);
+        newsAdapter.setList(list);
 
     }
     //轮播图数据
